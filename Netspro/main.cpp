dd187e6c26eecf5ae99810151a88fbf5c074791f
Hendrik Hanking <Hendrik.hanking@icloud.com>
2025-12-31 12:02:07 +0100

    Initial Commit

---
+++ Netspro/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Netspro
+//
+//  Created by Hendrik Hanking on 31.12.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
