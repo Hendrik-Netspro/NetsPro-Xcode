d78e4a3193f7814da4f24efdabbc24bd8c80c17b
Hendrik-Netspro <Hendrik.hanking@icloud.com>
2026-01-02 20:53:25 +0100

    Update XL

--- Netspro/main.cpp
+++ Netspro/main.cpp
@@ -3,56 +3,70 @@
 //  Netspro
 //
 //  Created by Hendrik Hanking on 31.12.25.
-//  COPYWEITE Hendrik Vincent Hanking 2025
-//  Alle rechte vorberhaten
+//  COPYRIGHT Hendrik Vincent Hanking 2025
+//  All rights reserved
 
 #include <iostream>
 #include <string>
 #include <map>
 #include <functional>
-#include <cstdlib>   // system
-#include <cstdio>    // std::perror
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <thread>
+#include <chrono>
 
-// ankündigungen
-void clear_terminal();
-void eingabe_abfragung();
+// declarations
+void terminalLeeren();
+void benutzereingabeAbfragen();
 void hauptbildschirm();
-void handel_input();
-std::string hole_seriennummer();
+void verarbeiteEingabe();
+std::string holeSeriennummer();
+void anmeldung();
+void tokenInitialisierer();
 
-//ende
-//variablen
-std::string input;
+// end
+// variables
+std::string eingabe;
 std::string version = "1.0.0a";
 std::string seriennummer;
+std::string benutzername;
+std::string passwort;
+std::string gebildetesToken;
 
-//ende
+// end
+// user tokens
+std::vector<std::string> tokenListe; // create list
+
+// end
+
 int main() {
-    clear_terminal();
-    seriennummer = hole_seriennummer();
-    hauptbildschirm();
-
+    terminalLeeren();
+    tokenInitialisierer();
+    seriennummer = holeSeriennummer();
+    anmeldung();
     while (true) {
-        eingabe_abfragung();
-        if (input == "exit") {
+        benutzereingabeAbfragen();
+        if (eingabe == "exit") {
             break;
         }
-        handel_input();
+        verarbeiteEingabe();
     }
 
     return 0;
 }
 
-std::string hole_seriennummer() {
+std::string holeSeriennummer() {
     std::string result;
 #if defined(_WIN32) || defined(_WIN64)
     char buffer[128];
     FILE* pipe = _popen("wmic bios get serialnumber", "r");
-    if (!pipe) return "Unbekannt";
+    if (!pipe) return "Unknown";
     while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
         std::string line(buffer);
         if (line.find("SerialNumber") == std::string::npos && !line.empty()) {
-            // Bereinige Zeilenumbruch
+            // Clean newline
             line.erase(line.find_last_not_of(" \n\r") + 1);
             if (!line.empty()) result = line;
         }
@@ -61,64 +75,103 @@ std::string hole_seriennummer() {
 #elif defined(__APPLE__)
     char buffer[256];
     FILE* pipe = popen("system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'", "r");
-    if (!pipe) return "Unbekannt";
+    if (!pipe) return "Unknown";
     if (fgets(buffer, sizeof(buffer), pipe)) {
         result = buffer;
-        // Entferne evtl. Zeilenumbruch
+        // Remove possible newline
         result.erase(result.find_last_not_of(" \n\r") + 1);
     }
     pclose(pipe);
 #elif defined(__linux__)
     char buffer[256];
     FILE* pipe = popen("cat /sys/class/dmi/id/product_serial", "r");
-    if (!pipe) return "Unbekannt";
+    if (!pipe) return "Unknown";
     if (fgets(buffer, sizeof(buffer), pipe)) {
         result = buffer;
         result.erase(result.find_last_not_of(" \n\r") + 1);
     }
     pclose(pipe);
 #else
-    result = "Unbekannt";
+    result = "Unknown";
 #endif
-    return result.empty() ? "Unbekannt" : result;
+    return result.empty() ? "Unknown" : result;
 }
 
-void clear_terminal() {
+void terminalLeeren() {
     #if defined(_WIN32) || defined(_WIN64)
         std::system("cls");
     #else
         std::system("clear");
     #endif
 }
-void eingabe_abfragung() {
-    std::cout << "NETSPECTREPROTERMINEL@" << seriennummer << ">>>" ;
-    std::getline(std::cin, input);
+void benutzereingabeAbfragen() {
+    std::cout << "NETSPECTREPROTERMINAL@" << seriennummer << ">>> ";
+    std::getline(std::cin, eingabe);
 }
 
 
-void handel_input() {
-    if (input == "info") {
-        std::cout << "NetSpectre Pro Terminel cpp edition" << std::endl;
+void verarbeiteEingabe() {
+    if (eingabe == "info") {
+        std::cout << "NetSpectre Pro Terminal C++ edition" << std::endl;
         std::cout << "V." << version << std::endl;
-        std::cout << "Seriennummer: " << seriennummer << std::endl;
+        std::cout << "Serial number: " << seriennummer << std::endl;
     }
-    else if (input == "cls" || input == "clear") {
-        clear_terminal();
+    else if (eingabe == "cls" || eingabe == "clear") {
+        terminalLeeren();
     }
-    else if (input == "exit") {
+    else if (eingabe == "exit") {
         // Do nothing here, main will handle exit
     }
     else {
-        std::cout << "Den command haben wir nicht gefunden. :(" << std::endl;
+        std::cout << "Command not found :(" << std::endl;
+    }
+}
+
+void tokenInitialisierer() {
+    tokenListe.push_back("Anna");
+    tokenListe.push_back("Hendrik.Hoppel10");
+}
+
+
+void anmeldung() {
+    std::cout << R"(   _  __    __    ____             __            ___  ___  ____ )" << std::endl;
+    std::cout << R"(  / |/ /__ / /_  / __/__  ___ ____/ /________   / _ \/ _ \/ __ \)" << std::endl;
+    std::cout << R"( /    / -_) __/ _\ \/ _ \/ -_) __/ __/ __/ -_) / ___/ , _/ /_/ /)" << std::endl;
+    std::cout << R"(/_/|_/\__/\__/ /___/ .__/\__/\__/\__/_/  \__/ /_/  /_/|_|\____/ )" << std::endl;
+    std::cout << R"(C++ EDITION       /_/                                           )" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Username: ";
+    std::getline(std::cin, benutzername);
+    terminalLeeren();
+    
+    std::cout << R"(   _  __    __    ____             __            ___  ___  ____ )" << std::endl;
+    std::cout << R"(  / |/ /__ / /_  / __/__  ___ ____/ /________   / _ \/ _ \/ __ \)" << std::endl;
+    std::cout << R"( /    / -_) __/ _\ \/ _ \/ -_) __/ __/ __/ -_) / ___/ , _/ /_/ /)" << std::endl;
+    std::cout << R"(/_/|_/\__/\__/ /___/ .__/\__/\__/\__/_/  \__/ /_/  /_/|_|\____/ )" << std::endl;
+    std::cout << R"(C++ EDITION       /_/                                           )" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Password for " << benutzername << ": ";
+    std::getline(std::cin, passwort);
+    gebildetesToken = benutzername + "." + passwort;
+    if (std::find(tokenListe.begin(), tokenListe.end(), gebildetesToken) != tokenListe.end()) {
+        hauptbildschirm();
+    } else {
+        std::cout << "Username or password not recognized." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(4));
+        terminalLeeren();
+        anmeldung();
     }
 }
 
 
+
+
+
 void hauptbildschirm() {
-    std::cout << "Willcommen Bei NETSPECTRE PRO cpp" << std::endl;
-    std::cout << "bis jetzt is Netspro cpp noch kostenlos" << std::endl;
-    std::cout << "wenn du einen kostenlosen accout willst Gebe feedback." << std::endl;
-    std::cout << "Feedback an:   Hendrik.Hanking@icloud.com." << std::endl;
+    std::cout << "Welcome to NETSPECTRE PRO C++" << std::endl;
+    std::cout << "So far Netspro C++ is still free" << std::endl;
+    std::cout << "If you want a free account, please provide feedback." << std::endl;
+    std::cout << "Feedback: Hendrik.Hanking@icloud.com." << std::endl;
     std::cout << R"(   _  __    __    ____             __            ___  ___  ____ )" << std::endl;
     std::cout << R"(  / |/ /__ / /_  / __/__  ___ ____/ /________   / _ \/ _ \/ __ \)" << std::endl;
     std::cout << R"( /    / -_) __/ _\ \/ _ \/ -_) __/ __/ __/ -_) / ___/ , _/ /_/ /)" << std::endl;
